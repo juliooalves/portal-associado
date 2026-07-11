25504582c9b07b4e3c93ba6f2176e91d22dbf31a
juliooalves <juliooalves73@gmail.com>
2026-07-10 22:02:15 -0300

    feat(scaffold): add MVC project, tests projects. editorconfig, build props

---
+++ src/ProAuto.PortalAssociado.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProAuto.PortalAssociado.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
